6719a0e2fd3ab058db4de83a5be6b1ceda039757
IStoum <ahmadakrom563@gmail.com>
2025-01-22 23:36:28 +0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://dzenacymjprywcjxoqke.supabase.co'; // URL Supabase Anda
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6ZW5hY3ltanByeXdjanhvcWtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5MTkxODIsImV4cCI6MjA1MjQ5NTE4Mn0.R_73IG7Nw3wWnUpc4NlsjWQ-BG0b1tUb1FEYY6AJn_Y'; // Gunakan kunci API dari dashboard Supabase Anda
+const SUPABASE_URI = "https://ixwifbqqxsyvmemapylm.supabase.co";
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4d2lmYnFxeHN5dm1lbWFweWxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5MTg1NDEsImV4cCI6MjA1MjQ5NDU0MX0._zZmRkTSEvrlOZ9BZoGP4rhCLGwgdr0F0qfjcs5ieRE';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URI, SUPABASE_KEY);
